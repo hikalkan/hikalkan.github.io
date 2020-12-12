f321a9af41404ef591038cdd96df5b30f3b5ab29
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-12-12 23:43:59 +0300

    fixed paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,23 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
-    "_includes/layouts/assets/css": "assets/css"
+    "src/_includes/layouts/assets/css": "assets/css"
   });
   eleventyConfig.addPassthroughCopy({
-    "_includes/layouts/assets/fontawesome": "assets/fontawesome"
+    "src/_includes/layouts/assets/fontawesome": "assets/fontawesome"
   });
   eleventyConfig.addPassthroughCopy({
-    "_includes/layouts/assets/images": "assets/images"
+    "src/_includes/layouts/assets/images": "assets/images"
   });
   eleventyConfig.addPassthroughCopy({
-    "_includes/layouts/assets/js": "assets/js"
+    "src/_includes/layouts/assets/js": "assets/js"
   });
   eleventyConfig.addPassthroughCopy({
-    "_includes/layouts/assets/plugins": "assets/plugins"
+    "src/_includes/layouts/assets/plugins": "assets/plugins"
   });
+  
+  return {
+    dir: {
+      input: "src"
+    }
+  };
 };
